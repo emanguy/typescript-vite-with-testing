9e601f2b8cbdb8c29fcf37e229319755d4b3b69a
Evan Rittenhouse <erittenhouse@cloudtamer.io>
2021-02-02 14:53:40 -0500

    Initial commit

---
+++ src/components/HelloWorld.test.ts
@@ -0,0 +1,8 @@
+import { shallowMount } from '@vue/test-utils';
+import HelloWorld from './HelloWorld.vue';
+
+describe('HelloWorld.vue', () => {
+	it('renders', () => {
+		shallowMount(HelloWorld);
+	})
+})
